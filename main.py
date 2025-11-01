95997d333833250ecdee82333025aeb56983b573
george <george.dfairman@gmail.com>
2025-11-01 09:57:00 -0400

    1

---
+++ main.py
@@ -0,0 +1,13 @@
+from stats import count_words
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    num_words = count_words(text)
+    print(f"Found {num_words} total words")
+
+if __name__ == "__main__":
+    main()
